6053793f509caefd659d30bf916a16b0d6818f64
aburayhan-bpi <aburayhan.bpi@gmail.com>
2024-09-20 16:48:58 +0600

    added transaction section and its functionality

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,10 +3,23 @@ document.getElementById('addMoneyBtn').addEventListener('click', function (event
     const addMoneyValue = getInputValueById('inputAmount');
     const pinValue = getInputValueById('pin');
 
+    if (isNaN(addMoneyValue)) {
+        alert('Failed to add money.')
+        return;
+    }
+
     if (pinValue === 1234) {
         const textValueNumber = getTextValueById('accBalance');
+
         const newBalance = addMoneyValue + textValueNumber;
         document.getElementById('accBalance').innerText = newBalance;
+
+        const p = document.createElement('p');
+        p.innerText = `Added: ${addMoneyValue}. New Balance: Tk. ${newBalance}`;
+        // console.log(p);
+
+        // we should use common function for this
+        document.getElementById('transaction-container').appendChild(p);
 
     }
     else {
--- js/cashOut.js
+++ js/cashOut.js
@@ -4,11 +4,30 @@ document.getElementById('cashOutBtn').addEventListener('click', function (event)
     const addMoneyValue = getInputValueById('cashOutAmount');
     const pinValue = getInputValueById('cashOutPin');
 
+    if (isNaN(addMoneyValue)) {
+        alert('Failed to add money.')
+        return;
+    }
+
     if (pinValue === 1234) {
         const balance = getTextValueById('accBalance');
+        if (addMoneyValue > balance) {
+            alert('Failed to add money');
+            return;
+        }
         const newBalance = balance - addMoneyValue;
         document.getElementById('accBalance').innerText = newBalance;
-        // console.log(newBalance);
+
+
+        const div = document.createElement('div');
+        div.classList.add('bg-amber-200')
+        div.innerHTML = `
+            <h2 class="font-bold">Cash out</h2>
+            <p>${addMoneyValue} withdrawn. Reamining balance: ${newBalance}</p>
+        
+        `
+        document.getElementById('transaction-container').appendChild(div);
+
     }
     else {
         alert('Failed to cash out.')
